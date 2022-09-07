c9173527dd6d6c9139427d7acd2e07c534c77f1e
joj38 <35536968+joj38@users.noreply.github.com>
2022-09-07 13:54:05 +0000

    jokull

--- src/main/java/is/hi/hbv501g/hugbunadarverkefni1/Hugbunadarverkefni1Application.java
+++ src/main/java/is/hi/hbv501g/hugbunadarverkefni1/Hugbunadarverkefni1Application.java
@@ -7,8 +7,11 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Hugbunadarverkefni1Application {
 
+    //Jökull joj38
+
     public static void main(String[] args) {
         SpringApplication.run(Hugbunadarverkefni1Application.class, args);
     }
+
 
 }
