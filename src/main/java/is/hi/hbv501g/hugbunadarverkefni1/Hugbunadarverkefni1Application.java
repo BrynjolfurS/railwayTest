b80af182b930d4d397859f9132e04cf8501798b3
joj38 <35536968+joj38@users.noreply.github.com>
2022-09-07 12:07:06 +0000

    initial commit

---
+++ src/main/java/is/hi/hbv501g/hugbunadarverkefni1/Hugbunadarverkefni1Application.java
@@ -0,0 +1,14 @@
+package is.hi.hbv501g.hugbunadarverkefni1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class Hugbunadarverkefni1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hugbunadarverkefni1Application.class, args);
+    }
+
+}
