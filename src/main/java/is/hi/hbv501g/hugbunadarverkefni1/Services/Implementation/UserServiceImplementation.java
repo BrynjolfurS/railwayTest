26fbb042ed947e24d8ebeb594092d4dfafd6e938
Oli <olp10@hi.is>
2022-09-21 18:24:24 +0000

    Setti upp það sem var gert í support session 1/2

---
+++ src/main/java/is/hi/hbv501g/hugbunadarverkefni1/Services/Implementation/UserServiceImplementation.java
@@ -0,0 +1,6 @@
+package is.hi.hbv501g.hugbunadarverkefni1.Services.Implementation;
+
+import is.hi.hbv501g.hugbunadarverkefni1.Services.UserService;
+
+public class UserServiceImplementation implements UserService {
+}
---
+++ src/main/java/is/hi/hbv501g/hugbunadarverkefni1/Services/UserService.java
@@ -0,0 +1,4 @@
+package is.hi.hbv501g.hugbunadarverkefni1.Services;
+
+public interface UserService {
+}
