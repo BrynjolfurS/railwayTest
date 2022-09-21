6ebf6f60452d410c14309002aeac27d53a807aab
Oli <olp10@hi.is>
2022-09-21 16:16:10 +0000

    asdf

--- src/main/java/is/hi/hbv501g/hugbunadarverkefni1/Hugbunadarverkefni1Application.java
+++ src/main/java/is/hi/hbv501g/hugbunadarverkefni1/Hugbunadarverkefni1Application.java
@@ -1,16 +1,16 @@
 package is.hi.hbv501g.hugbunadarverkefni1;
 
-// Ólafur Pálsson
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.*;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-public class Hugbunadarverkefni1Application {
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class Hugbunadarverkefni1Application implements ErrorController {
 
     public static void main(String[] args) {
         SpringApplication.run(Hugbunadarverkefni1Application.class, args);
     }
-
 }
